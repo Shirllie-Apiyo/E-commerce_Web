2c5f6df6faf910a4df5ab47c20cb84c2acab8b3b
Shirllie-Apiyo <apraise320@gmail.com>
2022-02-18 14:37:00 +0300

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './modules/products/products.component';
 import { HeaderComponent } from './modules/header/header.component';
+import { FooterComponent } from './modules/footer/footer.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'products', component: ProductsComponent },
   { path: 'header', component: HeaderComponent },
+  {path:'footer', component:FooterComponent},
 
 ];
 
